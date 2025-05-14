87b441888054ee10567f787528d84e412cbadf2f
tranvietanh20112001 <tranvietanh20112001@gmail.com>
2025-05-14 10:17:26 +0700

    fix menu button

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 import { Index } from './components'
 import { Exp } from './components/exp'
 import { Contact } from './components/contact'
-import { MenuButton } from '../../components/menuButton'
+import { MenuButton } from '../../components/MenuButton'
 export function Homepage() {
     return (
         <Box width={'100%'} height={'auto'} margin={'0 auto'}>
